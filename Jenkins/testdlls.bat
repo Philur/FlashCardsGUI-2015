2b9bbb7a92af2f061fa6f0baa3095b12dbfa0dcd
Philur <Philur@users.noreply.github.com>
2015-05-12 16:17:18 +0200

    Update testdlls.bat

--- Jenkins/testdlls.bat
+++ Jenkins/testdlls.bat
@@ -1,7 +1,7 @@
 SetLocal EnableDelayedExpansion
 echo ----- DIR
-dir /s *Tests.dll /b | find "release" /i
-dir /s *Tests.dll /b | find "release" /i > temp.txt
+dir /s *Tests.dll /b | find "release" /i | find "bin"
+dir /s *Tests.dll /b | find "release" /i | find "bin" > temp.txt
 echo ----- LIST
 set L=1
 for /F "delims=" %%a in (temp.txt) do (
@@ -10,3 +10,7 @@ for /F "delims=" %%a in (temp.txt) do (
 )
 echo %list%
 echo ----- 
+mkdir Dynamic
+cd Dymanic
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" %list% /logger:trx
+echo -----
